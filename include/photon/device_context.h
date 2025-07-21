18f199a0509f757db74da1b5fa54236a9d6ad112
Wenchao Huang <physhuangwenchao@gmail.com>
2025-07-22 00:55:43 +0800

    Implement `DeviceContext` class.
    Signed-off-by: Wenchao Huang <physhuangwenchao@gmail.com>

---
+++ include/photon/device_context.h
@@ -0,0 +1,97 @@
+﻿/**
+ *	Copyright (c) 2025 Wenchao Huang <physhuangwenchao@gmail.com>
+ *
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy
+ *	of this software and associated documentation files (the "Software"), to deal
+ *	in the Software without restriction, including without limitation the rights
+ *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *	copies of the Software, and to permit persons to whom the Software is
+ *	furnished to do so, subject to the following conditions:
+ *
+ *	The above copyright notice and this permission notice shall be included in all
+ *	copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *	SOFTWARE.
+ */
+#pragma once
+
+#include "fwd.h"
+
+namespace PHOTON_NAMESPACE
+{
+	/*****************************************************************************
+	******************************    DeviceProp    ******************************
+	*****************************************************************************/
+
+	struct DeviceProp
+	{
+		//	Optix 7.0.0
+		unsigned int	version;							//!	The RT core version supported by the device (0 for no support, 10 for version 1.0).
+		unsigned int	clusterAccel;						//!	Flag specifying support for cluster acceleration structure builds.
+		unsigned int	maxSbtOffset;						//!	The maximum value for OptixInstance::sbtOffset.
+		unsigned int	maxInstanceID;						//!	The maximum value for OptixInstance::instanceId.
+		unsigned int	maxTraceDepth;						//!	Maximum value for OptixPipelineLinkOptions::maxTraceDepth.
+		unsigned int	maxPrimitivesPerGAS;				//!	The maximum number of primitives (over all build inputs) as input to a single Geometry Acceleration Structure (GAS).
+		unsigned int	maxInstancesPerIAS;					//!	The maximum number for the sum of the number of SBT records of all build inputs to a single Geometry Acceleration Structure (GAS).
+		unsigned int	maxSbtRecordsPerGAS;				//!	The maximum number of instances that can be added to a single Instance Acceleration Structure (IAS).
+		unsigned int	maxTraversableGraphDepth;			//!	Maximum value to pass into optixPipelineSetStackSize.
+		unsigned int	numBitsInstanceVisiblityMask;		//!	The number of bits available for the OptixInstance::visibilityMask.
+		//	Optix 8.1.0
+		unsigned int	shaderExecutionReordering;			//!	Flag specifying capabilities of the optixReorder() device function.
+		//	Optix 9.0.0
+		unsigned int	cooperativeVector;					//!	Flag specifying whether cooperative vector support is enabled for this device.
+		unsigned int	maxClusterVertices;					//!	The maximum unique vertices per cluster in a cluster acceleration structure builds.
+		unsigned int	maxClusterTriangles;				//!	The maximum triangles per cluster in a cluster acceleration structure builds.
+		unsigned int	maxStructuredGridResolution;		//!	The maximum resolution per cluster in a structured cluster acceleration structure builds.
+	};
+
+	/*****************************************************************************
+	****************************    DeviceContext    *****************************
+	*****************************************************************************/
+
+	/**
+	 *	@brief		Abstract interface for an OptiX device context.
+	 * 
+	 *	This class represents a wrapper around an OptiX device context.
+	 *	It provides access to device-specific properties and the underlying device itself.
+	 *	Derived implementations are responsible for managing the actual OptiX context and
+	 *	associated resources such as modules, pipelines, and acceleration structures.
+	 */
+	class DeviceContext
+	{
+
+	public:
+
+		//!	@brief	Virtual destructor.
+		virtual ~DeviceContext() {}
+
+		//!	@brief		Return pointer to the device associated with.
+		virtual ns::Device * device() const = 0;
+
+		//!	@brief		Return pointer to the properties.
+		virtual const DeviceProp & properties() const = 0;
+
+	public:
+
+		/**
+		 *	@brief		Create a context for the device.
+		 *	@param[in]	device - Pointer to the device associated with.
+		 *	@param[in]	logLevel - Logging level for OptiX messages:
+		 *						  - 0: Disable logging
+		 *						  - 1: Fatal errors only
+		 *						  - 2: Errors and warnings
+		 *						  - 3: Errors, warnings, and informational messages (default)
+		 *						  - 4: Verbose output
+		 *	@param[in]	validationMode - Enable or disable validation mode (requires Optix version >= 7.2.0).
+		 *	@return		Return shared pointer to the newly created context.
+		 *	@throw		OptixResult - Throw `OptixResult` in case of failure.
+		 */
+		static std::shared_ptr<DeviceContext> create(ns::Device * device, int logLevel = 3, bool validationMode = false);
+	};
+}
---
+++ src/device_context_impl.cpp
@@ -0,0 +1,131 @@
+﻿/**
+ *	Copyright (c) 2025 Wenchao Huang <physhuangwenchao@gmail.com>
+ *
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy
+ *	of this software and associated documentation files (the "Software"), to deal
+ *	in the Software without restriction, including without limitation the rights
+ *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *	copies of the Software, and to permit persons to whom the Software is
+ *	furnished to do so, subject to the following conditions:
+ *
+ *	The above copyright notice and this permission notice shall be included in all
+ *	copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *	SOFTWARE.
+ */
+
+#include <nucleus/device.h>
+#include <nucleus/logger.h>
+#include "device_context_impl.h"
+
+#include <optix_stubs.h>
+#include <optix_function_table_definition.h>
+
+#if OPTIX_VERSION < 70000
+	#error "Requires Optix version >= 7.0.0!"
+#endif
+
+PHOTON_USING_NAMESPACE
+
+/*********************************************************************************
+*********************************    optixLog    *********************************
+*********************************************************************************/
+
+static void optixLog(unsigned int level, const char * tag, const char * msg, [[maybe_unused]] void * userData)
+{
+	switch (level)
+	{
+		case 1:		NS_ASSERT_LOG("[%s]: %s", tag, msg);		NS_ASSERT(false);		break;
+		case 2:		NS_ERROR_LOG("[%s]: %s", tag, msg);									break;
+		case 3:		NS_WARNING_LOG("[%s]: %s", tag, msg);								break;
+		case 4:		NS_INFO_LOG("[%s]: %s", tag, msg);									break;
+		default:																		break;
+	}
+};
+
+/*********************************************************************************
+******************************    DeviceContext    *******************************
+*********************************************************************************/
+
+std::shared_ptr<DeviceContext> DeviceContext::create(ns::Device * device, int logLevel, [[maybe_unused]] bool validationMode)
+{
+	OptixResult err = optixInit();
+
+	if (err == OPTIX_SUCCESS)
+	{
+		device->init();
+
+		OptixDeviceContext								hContext = nullptr;
+		OptixDeviceContextOptions						deviceContextOptions = {};
+	#if OPTIX_VERSION >= 70200
+		deviceContextOptions.validationMode				= validationMode ? OPTIX_DEVICE_CONTEXT_VALIDATION_MODE_ALL : OPTIX_DEVICE_CONTEXT_VALIDATION_MODE_OFF;
+	#endif
+		deviceContextOptions.logCallbackLevel			= NS_MIN(NS_MAX(0, logLevel), 4);	//!	0 -> Disable, 1 -> Fatal, 2 -> Error, 3 -> Warning, 4 -> Info.
+		deviceContextOptions.logCallbackFunction		= optixLog;
+		deviceContextOptions.logCallbackData			= device;
+
+		err = optixDeviceContextCreate(nullptr, &deviceContextOptions, &hContext);
+
+		if (err == OPTIX_SUCCESS)
+		{
+			DeviceProp devProp = {};
+
+		#if (OPTIX_VERSION >= 70000)
+			optixDeviceContextGetProperty(hContext, OPTIX_DEVICE_PROPERTY_RTCORE_VERSION, &devProp.version, sizeof(DeviceProp::version));
+			optixDeviceContextGetProperty(hContext, OPTIX_DEVICE_PROPERTY_LIMIT_MAX_SBT_OFFSET, &devProp.maxSbtOffset, sizeof(DeviceProp::maxSbtOffset));
+			optixDeviceContextGetProperty(hContext, OPTIX_DEVICE_PROPERTY_LIMIT_MAX_TRACE_DEPTH, &devProp.maxTraceDepth, sizeof(DeviceProp::maxTraceDepth));
+			optixDeviceContextGetProperty(hContext, OPTIX_DEVICE_PROPERTY_LIMIT_MAX_INSTANCE_ID, &devProp.maxInstanceID, sizeof(DeviceProp::maxInstanceID));
+			optixDeviceContextGetProperty(hContext, OPTIX_DEVICE_PROPERTY_LIMIT_MAX_INSTANCES_PER_IAS, &devProp.maxInstancesPerIAS, sizeof(DeviceProp::maxInstancesPerIAS));
+			optixDeviceContextGetProperty(hContext, OPTIX_DEVICE_PROPERTY_LIMIT_MAX_PRIMITIVES_PER_GAS, &devProp.maxPrimitivesPerGAS, sizeof(DeviceProp::maxPrimitivesPerGAS));
+			optixDeviceContextGetProperty(hContext, OPTIX_DEVICE_PROPERTY_LIMIT_MAX_SBT_RECORDS_PER_GAS, &devProp.maxSbtRecordsPerGAS, sizeof(DeviceProp::maxSbtRecordsPerGAS));
+			optixDeviceContextGetProperty(hContext, OPTIX_DEVICE_PROPERTY_LIMIT_MAX_TRAVERSABLE_GRAPH_DEPTH, &devProp.maxTraversableGraphDepth, sizeof(DeviceProp::maxTraversableGraphDepth));
+			optixDeviceContextGetProperty(hContext, OPTIX_DEVICE_PROPERTY_LIMIT_NUM_BITS_INSTANCE_VISIBILITY_MASK, &devProp.numBitsInstanceVisiblityMask, sizeof(DeviceProp::numBitsInstanceVisiblityMask));
+		#endif
+		#if (OPTIX_VERSION >= 80100)
+			optixDeviceContextGetProperty(hContext, OPTIX_DEVICE_PROPERTY_SHADER_EXECUTION_REORDERING, &devProp.shaderExecutionReordering, sizeof(DeviceProp::shaderExecutionReordering));
+		#endif
+		#if (OPTIX_VERSION >= 90000)
+			optixDeviceContextGetProperty(hContext, OPTIX_DEVICE_PROPERTY_CLUSTER_ACCEL, &devProp.clusterAccel, sizeof(DeviceProp::clusterAccel));
+			optixDeviceContextGetProperty(hContext, OPTIX_DEVICE_PROPERTY_COOP_VEC, &devProp.cooperativeVector, sizeof(DeviceProp::cooperativeVector));
+			optixDeviceContextGetProperty(hContext, OPTIX_DEVICE_PROPERTY_LIMIT_MAX_CLUSTER_VERTICES, &devProp.maxClusterVertices, sizeof(DeviceProp::maxClusterVertices));
+			optixDeviceContextGetProperty(hContext, OPTIX_DEVICE_PROPERTY_LIMIT_MAX_CLUSTER_TRIANGLES, &devProp.maxClusterTriangles, sizeof(DeviceProp::maxClusterTriangles));
+			optixDeviceContextGetProperty(hContext, OPTIX_DEVICE_PROPERTY_LIMIT_MAX_STRUCTURED_GRID_RESOLUTION, &devProp.maxStructuredGridResolution, sizeof(DeviceProp::maxStructuredGridResolution));
+		#endif
+
+			NS_INFO_LOG("Creating Optix context on device(%d) successfully, RT-Core version: %d.", device->id(), devProp.version);
+
+			return std::make_shared<DeviceContextImpl>(device, hContext, devProp);
+		}
+	}
+
+	NS_ERROR_LOG("Failed to create Optix context on device(%d): %s.", device->id(), optixGetErrorString(err));
+
+	throw err;
+}
+
+/*********************************************************************************
+****************************    DeviceContextImpl    *****************************
+*********************************************************************************/
+
+DeviceContextImpl::DeviceContextImpl(ns::Device * device, OptixDeviceContext hContext, const DeviceProp & devProp)
+	: m_device(device), m_hContext(hContext), m_devProp(devProp)
+{
+
+}
+
+
+DeviceContextImpl::~DeviceContextImpl()
+{
+	if (m_hContext != nullptr)
+	{
+		OptixResult err = optixDeviceContextDestroy(m_hContext);
+
+		NS_ERROR_LOG_IF(err != OPTIX_SUCCESS, "%s.", optixGetErrorString(err));
+	}
+}
